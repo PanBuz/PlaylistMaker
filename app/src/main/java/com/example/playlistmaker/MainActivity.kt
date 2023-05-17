907f4e55e5a6523edb171b95b3eed9768655da63
AlSerPan <aleksandor1203@yandex.ru>
2023-05-18 02:09:47 +0500

    Second commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settings = findViewById<Button>(R.id.settingB)
+        val search = findViewById<Button>(R.id.searchB)
+        val librar = findViewById<Button>(R.id.libraryB)
+
+        settings.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+        search.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        librar.setOnClickListener {
+            val librarIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(librarIntent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backSet = findViewById<Button>(R.id.backSettings)
+
+        backSet.setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+
+    }
+}
