8c1b50cd43f3ba2ebe3deca8ad97a0847e6fcdc8
AlSerPan <aleksandor1203@yandex.ru>
2023-05-21 19:21:08 +0500

    Исправил замечания

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val settings = findViewById<Button>(R.id.settingB)
-        val search = findViewById<Button>(R.id.searchB)
-        val librar = findViewById<Button>(R.id.libraryB)
+        val settings = findViewById<Button>(R.id.settingButton)
+        val search = findViewById<Button>(R.id.searchButton)
+        val librar = findViewById<Button>(R.id.libraryButton)
 
         settings.setOnClickListener {
             val settingsIntent = Intent(this, SettingsActivity::class.java)
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -3,19 +3,22 @@ package com.example.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Button
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backSet = findViewById<Button>(R.id.backSettings)
+        val backButton = findViewById<Button>(R.id.backSettings)
 
-        backSet.setOnClickListener {
+        backButton.setOnClickListener {
             val mainIntent = Intent(this, MainActivity::class.java)
             startActivity(mainIntent)
+            finish()
         }
-
     }
 }
