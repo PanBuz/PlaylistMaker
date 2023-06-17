cad0266b9ff2582cd283b8e9487b65a03a10ff87
AlSerPan <aleksandor1203@yandex.ru>
2023-06-17 17:11:12 +0500

    Рабочая версия, пока без поиска

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
+import android.net.Uri
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val courseUrl = "https://yandex.ru/praktikum"
+    private val studentEmail = "aleksandor1203@yandex.ru"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<Button>(R.id.backSettings)
+        val shareButton = findViewById<Button>(R.id.shareBtm)
+        val supportButton = findViewById<Button>(R.id.supportBtm)
+        val licenceBottom: Button = findViewById(R.id.licenceBtm)
 
         backButton.setOnClickListener {
             val mainIntent = Intent(this, MainActivity::class.java)
@@ -21,5 +28,52 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        // Обработчик нажатия на кнопку "Поделиться приложением"
+        shareButton.setOnClickListener {
+            shareCourse()
+        }
+
+        supportButton.setOnClickListener {
+            sendEmailToSupport()
+        }
+
+
+        licenceBottom.setOnClickListener {
+            val url = "https://praktikum.yandex.ru/oferta/"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
     }
+
+    private fun shareCourse() {
+        // Создание интента для отправки данных
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, courseUrl)
+            type = "text/plain"
+        }
+
+        // Запуск стандартного системного sharing-диалога
+        startActivity(Intent.createChooser(sendIntent, "Поделиться приложением"))
+    }
+
+    private fun sendEmailToSupport() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(studentEmail))
+            putExtra(
+                Intent.EXTRA_SUBJECT,
+                "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            )
+            putExtra(
+                Intent.EXTRA_TEXT,
+                "Спасибо разработчикам и разработчицам за крутое приложение!"
+            )
+        }
+
+        startActivity(intent)
+    }
+
 }
