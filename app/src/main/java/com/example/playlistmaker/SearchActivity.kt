86bc80f9b1bb29c097b5606c6723c20eec08ecf2
AlSerPan <aleksandor1203@yandex.ru>
2023-07-13 01:53:26 +0500

    Проделал поистене большую работу... Исправил манифест, подключил библиотеку Glide, создал viewHolder, подключил адаптер, подогнал макет...

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -7,12 +7,15 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.FrameLayout
-import android.widget.ImageView
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class SearchActivity : AppCompatActivity() {
     private var inputEditText: String = ""
@@ -25,6 +28,43 @@ class SearchActivity : AppCompatActivity() {
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
         val backButton = findViewById<Button>(R.id.backButton)
+
+
+        val trackAdapter = TrackAdapter(
+            listOf(
+                Track("Smells Like Teen Spirit", "Nirvana", "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Billie Jean",
+                "Michael Jackson",
+                "4:35",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Stayin' Alive",
+                "Bee Gees",
+                "4:10",
+                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Whole Lotta Love",
+                "Led Zeppelin",
+                "5:33",
+                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+            ),
+            Track(
+                "Sweet Child O'Mine",
+                "Guns N' Roses",
+                "5:03",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+                )
+            )
+        )
+
+        val rvTrack = findViewById<RecyclerView>(R.id.rvTrack)
+        rvTrack.adapter = trackAdapter
+        rvTrack.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         backButton.setOnClickListener {
             finish()
@@ -85,3 +125,49 @@ class SearchActivity : AppCompatActivity() {
     }
 
 }
+
+data class Track(
+    val trackName: String, // Название композиции
+    val artistName: String, // Имя исполнителя
+    val trackTime: String, // Продолжительность трека
+    val artworkUrl100: String, // Ссылка на изображение обложки
+)
+
+class TrackAdapter(
+    private val trackData: List<Track>
+) : RecyclerView.Adapter<TrackViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.treck_item, parent, false)
+        return TrackViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(trackData[position])
+    }
+
+    override fun getItemCount(): Int {
+        return trackData.size
+    }
+
+}
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val tvTrackName: TextView = itemView.findViewById(R.id.tvTrackName)
+    private val tvArtistName: TextView = itemView.findViewById(R.id.tvArtistName)
+    private val tvTrackTime: TextView = itemView.findViewById(R.id.tvTimeTrack)
+    private val ivTrackImage: ImageView = itemView.findViewById(R.id.ivPlaceHolderImage)
+
+    fun bind(item: Track) {
+        Glide.with(ivTrackImage).load(item.artworkUrl100)
+            .centerInside()
+            .transform(RoundedCorners(4))
+            .placeholder(R.drawable.placeholder)
+            .into(ivTrackImage)
+
+
+        tvTrackName.text = item.trackName
+        tvArtistName.text = item.artistName
+        tvTrackTime.text = item.trackTime
+    }
+}
+
---
+++ app/src/main/java/com/example/playlistmaker/SearchMysicVievHolder.kt
@@ -0,0 +1,17 @@
+package com.example.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.RoundedCorner
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+
+class SearchMysicVievHolder {
+
+}
+
