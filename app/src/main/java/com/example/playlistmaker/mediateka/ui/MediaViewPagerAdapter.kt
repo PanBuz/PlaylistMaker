422f3378f370ec3f0d7afbf9876c5c37db1b01cf
PanBuz <aleksandor1203@yandex.ru>
2024-01-16 03:17:42 +0500

    Исправил замечания

--- app/src/main/java/com/example/playlistmaker/mediateka/ui/MediaViewPagerAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/mediateka/ui/MediaViewPagerAdapter.kt
@@ -5,17 +5,14 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
+private const val MEDIA_SCREEN_TABS_COUNT : Int = 2
 class MediaViewPagerAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
-    override fun getItemCount() = itemCountFragment
+    override fun getItemCount() = MEDIA_SCREEN_TABS_COUNT
     override fun createFragment(position: Int): Fragment {
         return when(position) {
             0 -> FavoriteFragment.newInstance()
             else -> PlaylistFragment.newInstance()
         }
-    }
-
-    companion object {
-        val itemCountFragment : Int = 2
     }
 }
